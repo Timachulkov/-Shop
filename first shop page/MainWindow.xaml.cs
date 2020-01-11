cd9ba249207d6f3bbb665a8aac8590018dcbf4f5
Tima <timachulkov@mail.ru>
2020-01-11 14:52:31 +0500

    new look agan

--- first shop page/MainWindow.xaml.cs
+++ first shop page/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace first_shop_page
         {
             InitializeComponent();
         }
+
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
